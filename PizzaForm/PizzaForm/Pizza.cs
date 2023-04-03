15f6cdc5e59e717222d6b6e9494c894dca678613
Ahmet <Ahmet@DESKTOP-GM6KUI1>
2023-04-03 21:40:10 +0300

    updated

---
+++ PizzaForm/PizzaForm/Pizza.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaForm
+{
+    internal class Pizza
+    {
+        public string  Adi { get; set; }
+        public decimal Fiyat { get; set; }
+        public Ebat Ebati { get; set; }
+        public KenarTip KenarTipi { get; set; }
+
+        public List<string> Malzemeler { get; set; }
+        public override string ToString()
+        {
+            return Adi;
+        }
+
+
+
+
+
+    }
+}
